45563e9b98f3ca21c09f4bd425732ab5aa163180
Geoffrey Meier <grm0809@gmail.com>
2023-03-27 15:47:54 -0500

    Finished Assignment

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -1,6 +1,9 @@
 package org.launchcode.techjobs.persistent.controllers;
 
+import org.dom4j.rule.Mode;
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -10,9 +13,16 @@ import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("employers")
+@RequestMapping(value = "employers")
 public class EmployerController {
-
+@Autowired
+    private EmployerRepository employerRepository;
+@GetMapping("")
+public String index(Model model){
+    model.addAttribute("title","All Employers");
+    model.addAttribute("employers",employerRepository.findAll());
+    return "employers/index";
+}
 
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
@@ -27,14 +37,14 @@ public class EmployerController {
         if (errors.hasErrors()) {
             return "employers/add";
         }
-
+employerRepository.save(newEmployer);
         return "redirect:";
     }
 
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
 
-        Optional optEmployer = null;
+        Optional optEmployer = employerRepository.findById(employerId);
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,37 @@
 package org.launchcode.techjobs.persistent.models;
 
-public class Skill extends AbstractEntity {
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
+@Entity
+public class Skill extends AbstractEntity {
+    @NotBlank(message = "description is required")
+    @Size(min = 4, max = 100)
+    private String description;
+@ManyToMany(mappedBy = "skills")
+    private List<Job> jobs=new ArrayList<>();
+    public Skill() {}
+
+
+    public List<Job> getJobs() {
+
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
